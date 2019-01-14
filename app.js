1e83f65a70c87271f7a1bbeb566bd4e4dd69f159
Kayla Johnson <kd_johnson89@mac.com>
2019-01-14 17:37:12 -0600

    establish variables from form elements

---
+++ app.js
@@ -0,0 +1,5 @@
+var bill = document.getElementById('bill_amount').value;
+var input_percent = document.getElementById('tip_amount').value;
+var ten = document.getElementById('ten');
+var fifteen = document.getElementById('fifteen');
+var twenty = document.getElementById('twenty');
