1a8a9259a4851d0bc0c751fa335ae65f6443166e
Kayla Johnson <kd_johnson89@mac.com>
2019-01-14 18:11:01 -0600

    added tip calculating functionality for ten variable

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ var input_percent = document.getElementById('tip_amount').value;
 var ten = document.getElementById('ten');
 var fifteen = document.getElementById('fifteen');
 var twenty = document.getElementById('twenty');
+
+ten.addListener('click', function() {
+								var total = document.getElementById('total');
+								bill += bill * 0.10;
+								total = bill;
+								return total;
+								})
