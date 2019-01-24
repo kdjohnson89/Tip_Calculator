cbc8c8bd00f48bdec98b9c5dff44da4bcd372ca5
Kayla Johnson <kd_johnson89@mac.com>
2019-01-23 20:33:07 -0600

    change .js file into a function containing all variables and created new variables

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 document.getElementById('container').onchange = function() {
-	var bill = document.getElementById('billAmount').value;
+	var bill = Number(document.getElementById('billAmount').value);
 	var tip = document.getElementById('tipAmount').value;
-	var total = bill * (tip/100);
-	console.log(tip);
+	var split = document.getElementById('split').value;
+	var tipTotal = bill * (tip / 100);
+	var perPerson = (bill + tipTotal) / split;
+	var final = perPerson * split;
+	
+	document.getElementById('percent').innerHTML = tip + "%";
+	document.getElementById('people').innerHTML = split;
+	document.getElementById('total').innerHTML = "$" + perPerson;
+	document.getElementById('final').innerHTML= "$" + final;
 }
 
